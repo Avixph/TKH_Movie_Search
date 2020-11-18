08c468fa41223afa5e0b956c321febf9f49c1895
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2020-11-18 14:05:39 -0500

    Second commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { search } from "./api";
+import { appendMovies, clearMovies, setMessage } from "./ui";
 
 (() => {
   const handleSearchButtonClick = () => {
     const searchTerm = document.getElementById("search-pane-input").nodeValue;
 
+    clearMovies();
+    setMessage("Searching, please wait.");
+
     search(searchTerm)
       .then((response) => {
-        console.log(response);
+        if (res.Response == "True") {
+          console.log(res);
+          appendMovies(res.Search);
+          setMessage();
+        } else {
+          setMessage(
+            "Error: Could not find any matches, please refine your search term."
+          );
+        }
+        // console.log(response);
       })
       .catch((error) => {
-        console.log(error);
+        // console.log(error);
+        setMessage("Unexpected error occured, please try again.");
       });
   };
 
